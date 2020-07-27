e669a7bc2bdf1445a6cea1705e3efbfb50ffca10
Neil A. Dobson <neil.dobson71@gmail.com>
2020-07-27 19:47:09 +1000

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,16 @@
+export default {
+  target: "static",
+  components: true,
+  /*
+   ** Nuxt.js modules
+   ** Doc: https://nuxtjs.org/guide/modules
+   */
+  modules: [
+    // Doc: https://content.nuxtjs.org
+    "@nuxt/content"
+  ],
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/tailwindcss-module
+    "@nuxtjs/tailwindcss"
+  ]
+};
